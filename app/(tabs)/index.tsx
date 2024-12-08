05882e81c2314a303008c44d229d386c0b65b3ea
Hải Mai Văn <maivanhai061204dn@Hais-Mac-mini.local>
2024-12-08 18:52:20 +0700

    add my-app

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,7 @@
-import { Image, StyleSheet, Platform, View, Text, ImageBackground, TextInput, Button, Alert } from 'react-native';
+import { Image, StyleSheet, Platform, View, Text, ImageBackground, TextInput, Button, TouchableOpacity, Alert} from 'react-native';
 import React from 'react'
 import { useState } from 'react'
+import Icon from '@expo/vector-icons/FontAwesome'
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
@@ -10,12 +11,26 @@ import { red } from 'react-native-reanimated/lib/typescript/Colors';
 
 
 export default function HomeScreen() {
-  const image = { uri: 'https://img.lovepik.com/background/20211029/medium/lovepik-technological-mobile-wallpaper-background-image_400309739.jpg' }
+  const image = require('/Users/maivanhai061204dn/Documents/workspace/test-one/hello-you/my-app/assets/images/backgroundHouse.png')
   const [username, setUserName] = useState('')
   const [password, setPassword] = useState('')
+  
   const handleOnPress = () => {
-    console.log('username', username);
+    // check userName 
+    const checkSpace = /\s/;
+    const checkAccents = /[[áàảạãâầấẩậăắẳặâ]/i
+    if (checkSpace.test(username)) {
+      Alert.alert('Username khong duoc chua khoang trang')
+    } else if (checkSpace.test(username)) {
+      Alert.alert('Username khong duoc chua dau')
+    } else {
+      console.log('username', username);
+    }
     console.log('password', password);
+  }
+  const [passwordVisible, setPasswordVisible] = useState(false)
+  const handlePasswordVisible = () => {
+    setPasswordVisible(prevState => !prevState)
   }
   
     return (
@@ -26,28 +41,46 @@ export default function HomeScreen() {
             <Text style = {styles.text}>SMART HOME</Text>
           </View>
           <View style = {styles.logoContainer}>
-            <Image style = {styles.logo} source={{uri: 'https://media.istockphoto.com/id/1339031100/vector/art-illustration.jpg?s=612x612&w=is&k=20&c=JbgDGa81XKRdx4rzrgtKQFPDt_-9gpe-8Oryo1whdas=' }} />
+            <Image style = {styles.logo} source={require('/Users/maivanhai061204dn/Documents/workspace/test-one/hello-you/my-app/assets/images/logo-smart-home.png')} />
           </View>
           <View style= {styles.inputContainer}>
-            <View style= {styles.inputItiem}>
-              <Text>
-                User Name
-              </Text>
-              <TextInput placeholder='Enter UserName' onChangeText={setUserName} placeholderTextColor = 'grey' style = {styles.textInput} />
-            </View>
             <View>
-              <Text style= {styles.inputItiem}>
-                Password
-              </Text>
-              <TextInput placeholder='Enter PassWord' onChangeText={setPassword} placeholderTextColor = 'grey' style = {styles.textInput} />
+              <View style={styles.item}>
+                <Icon name='user' size={40} style={{marginEnd:10, fontSize: 20, color: 'white'}} />
+                <Text style= {styles.inputText}>
+                USER NAME
+                </Text>
+              </View>
+              <View style = {styles.textInput}>
+                <TextInput style={{flex: 1, color:'white', marginStart: 5}} placeholder='Enter UserName' onChangeText={setUserName} placeholderTextColor = 'rgba(255, 255, 255, 0.5)'  />
+              </View>
+            </View>
+            <View >
+              <View style={styles.item}>
+                <Icon name='lock' size={40} style={{marginEnd:10, fontSize: 20, color: 'white'}} />
+                <Text style= {styles.inputText}>
+                PASSWORD
+                </Text>
+              </View> 
+              <View  style = {styles.textInput}> 
+                
+                <TextInput style={{flex: 1, color:'white', marginStart: 5}} placeholder='Enter PassWord' onChangeText={setPassword} placeholderTextColor = 'rgba(255, 255, 255, 0.5)' secureTextEntry={!passwordVisible}  />
+                <TouchableOpacity onPress={handlePasswordVisible}>
+                  {passwordVisible ? (
+                      <Icon name='eye' size={20} color={'white'} marginEnd='10' ></Icon>
+                    ):(
+                      <Icon name='eye-slash' size={20} color={'white'} marginEnd='10' ></Icon>
+                    )}
+                </TouchableOpacity>
+
+              </View>
             </View>
           </View>
           <View style = {styles.buttonContainer}>
-            <Button color = 'white' title='onPress' onPress={handleOnPress}/>
+            <Button color = 'white' title='Login' onPress={handleOnPress}/>
           </View>
         </View>  
       </ImageBackground>
-      
     );
 }
 
@@ -55,14 +88,16 @@ const styles = StyleSheet.create({
   imageBackground: {
     flex: 1,
     height: '100%',
-    width: '100%'
+    width: '100%',
+    
   },
   container: {
     flex: 1,
     padding: 20,
     flexDirection: 'column',
     justifyContent: 'space-between',
-    
+    fontFamily: 'Arial',
+    backgroundColor: 'rgba(0, 0, 0, 0.4)'
   },
   textContainer: {
     flex: 0.5,
@@ -77,8 +112,9 @@ const styles = StyleSheet.create({
   },
   logoContainer: {
     flex: 2,
+    marginBottom: 70,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   logo: {
     height: 250,
@@ -89,23 +125,36 @@ const styles = StyleSheet.create({
     marginTop: 10,
 
   },
-  inputItiem: {
+  item:{
+    flexDirection: 'row',
+    alignItems: 'center',
     marginTop: 10
+  },
+  inputText: {
+    color: 'white',
+    fontWeight: 'bold',
   },
   textInput: {
     height: 50,
-    borderWidth: 1,
+    borderWidth: 2,
+    borderColor: 'rgba(0, 0, 0, 0.4)',
+    backgroundColor:'rgba(255, 255, 255, 0.3)',
     marginTop: 5,
     paddingStart: 5,
     fontSize: 15,
-    borderRadius: 10
+    borderRadius: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    
   },
   buttonContainer: {
     flex: 0.35,
+    borderColor: 'gray',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
     justifyContent: 'center',
     alignItems: 'center',
     borderWidth: 1,
-    backgroundColor: 'grey',
     marginBottom: 90,
     borderRadius: 10
   },
